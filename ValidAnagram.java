77390a19e261c8862228ee23fd7b1dad294a8de7
sravanDwadasi <sravandwadasi@gmail.com>
2024-12-29 12:19:12 +0530

    Added ValidAnagram

---
+++ ValidAnagram.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charS = s.toCharArray(); 
+        char[] charT = t.toCharArray(); 
+
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+        
+        return Arrays.equals(charS, charT);
+    }
+}
