af448e76c36ae7e92476f1f0fdfdbc17df39efeb
sravanDwadasi <sravandwadasi@gmail.com>
2024-12-31 21:59:10 +0530

    Added Single Element in a Sorted Array

--- SingleElementInASortedArray.java
+++ SingleElementInASortedArray.java
@@ -1,30 +1,3 @@
-# Daily LeetCode challenge
-
-## Day - 2 : 29-12-2024
-
-### 1. Replace Elements with Greatest Element on Right Side
-- **Medium**
-
-```
-public class ReplaceElementswithGreatestElementonRightSide {
-    public int[] replaceElements(int[] arr) {
-        int last = -1;
-        int preLast;
-        for(int i = arr.length-1; i >= 0; i--){
-            preLast = arr[i];
-            arr[i] = last;
-            if(preLast > last)last = preLast;
-        }
-        return arr;
-    }
-}
-
-```
-
-### 2. Single Element in a Sorted Array
-- **Medium**
-
-```
 class SingleElementInASortedArray {
     public int singleNonDuplicate(int[] nums) {
         if(nums.length == 1) return nums[0];
@@ -48,5 +21,3 @@ class SingleElementInASortedArray {
         return -1;
     }
 }
-
-```
