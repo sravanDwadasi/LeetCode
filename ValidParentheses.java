a04cb4c501fe932b081687680e6fe8420aa88a20
sravanDwadasi <sravandwadasi@gmail.com>
2025-01-18 15:58:59 +0530

    Added Valid Parentheses

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,3 +1,18 @@
+import java.util.Stack;
+
 public class ValidParentheses {
-    
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        if(s.length() == 1 || s.isEmpty()) return false;
+
+        for(char c : s.toCharArray()){
+            if(c == '(') stk.push(')');
+            else if(c == '{') stk.push('}');
+            else if(c == '[') stk.push(']');
+
+            else if(stk.isEmpty() || stk.pop() != c) return false;
+        }
+        return stk.isEmpty();
+    }
 }
