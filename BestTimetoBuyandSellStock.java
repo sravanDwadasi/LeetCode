80df22cda4b4dea3600c31b8995897a5d0216323
sravanDwadasi <sravandwadasi@gmail.com>
2025-01-18 18:12:01 +0530

    Added Best Time to Buy and Sell Stock

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,3 +1,12 @@
 public class BestTimetoBuyandSellStock {
+        public int maxProfit(int[] prices) {
+            int minPrice = Integer.MAX_VALUE;
+            int maxPrice = 0;
     
-}
+            for(int currentPrice : prices){
+                minPrice = Math.min(currentPrice, minPrice);
+                maxPrice = Math.max(maxPrice, currentPrice - minPrice);
+            }
+            return maxPrice;
+        }
+    }
